3ef80bfa8d8cb726ea65f5c53f9bff4b2d7d3de1
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2017-09-26 14:44:03 +0200

    remove starkit dependency

--- starkit/fix_spectrum1d.py
+++ starkit/fix_spectrum1d.py
@@ -1,7 +1,14 @@
-from specutils.spectrum1d import Spectrum1D as SUSpectrum1D
+#from specutils.spectrum1d import Spectrum1D as SUSpectrum1D
 from astropy import units as u
 
-class Spectrum1D(SUSpectrum1D):
+class Spectrum1D(object):
+
+
+    def __init__(self, wavelength, flux, uncertainty):
+        self.wavelength = wavelength
+        self.flux = flux
+        self.uncertainty = uncertainty
+
 
     def uncertainty_getter(self):
         return self._uncertainty
