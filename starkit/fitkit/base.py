41716f66497098f965b95500be9b278124e9bae7
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2018-06-02 13:12:55 +0200

    add new functionality to parameter explorer

--- starkit/fitkit/base.py
+++ starkit/fitkit/base.py
@@ -20,34 +20,61 @@ class ParameterExplorer(object):
         self.priors.update(self._generate_grid_uniform_priors())
 
         self.full_likelihood = model | likelihood
+
         self.param_names_raw = self.full_likelihood.param_names
-        self.param_names = [item[:item.rfind('_')]
-                            for item in self.param_names_raw]
-        self.param_fixed = np.array([getattr(self.full_likelihood, param_name).fixed
-                            for param_name in self.param_names_raw])
-        self.param_values = np.array([getattr(self.full_likelihood, param_name).value
-                            for param_name in self.param_names_raw])
+
+        self.param_names = np.array([item[:item.rfind('_')]
+                                     for item in self.param_names_raw])
+        self.param_fixed = np.array(
+            [getattr(self.full_likelihood, param_name).fixed
+             for param_name in self.param_names_raw])
+
+        self.param_values = np.array(
+            [getattr(self.full_likelihood, param_name).value
+             for param_name in self.param_names_raw])
 
 
     def _generate_grid_uniform_priors(self):
+        """
+        Get the uniform priors from the grid
+        """
         spectral_grid = self.model[0]
         param_names = spectral_grid.param_names
         bounds = spectral_grid.get_grid_extent()
         priors = {pname:stats.uniform(loc=bound[0], scale=bound[1]-bound[0]).ppf
                   for pname, bound in zip(param_names, bounds)}
+
+
         return priors
 
     def update_prior_transform(self):
-        pass
+        self._prior_transform_list = [self.priors[pname]
+                                     for pname in self.active_param_names]
+
+
+    def prior_transform(self, parameters):
+        transformed_params = np.array(
+            [transform(param) for transform, param in
+             zip(self._prior_transform_list, parameters)])
+        return transformed_params
+
 
     @property
     def model_param_values(self):
         return self.param_values[:len(self.model.param_names)]
 
     @property
+    def n_model_param(self):
+        return len(self.model_param_values)
+
+    @property
     def model_param_fixed(self):
         return self.param_fixed[:len(self.model.param_names)]
 
+    @property
+    def active_param_names(self):
+        return [pname for pname, fixed in
+                zip(self.param_names, self.param_fixed) if not fixed]
 
     def model_eval(self, *params):
         model_parameters = self.model_param_values.copy()
