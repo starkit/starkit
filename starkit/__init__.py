e2bde0888ec64d409423b67bc3815917f9c140b2
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2018-10-19 16:19:01 -0500

    add colored logger

--- starkit/__init__.py
+++ starkit/__init__.py
@@ -4,7 +4,7 @@
 This is an Astropy affiliated package.
 """
 
-import os
+import os, sys
 on_rtd = os.environ.get('READTHEDOCS') == 'True'
 from ._astropy_init import *
 
@@ -16,15 +16,23 @@ if not on_rtd:
     # ----------------------------------------------------------------------------
 
     # For egg_info test builds to pass, put package imports here.
+    from starkit.utils.colored_logger import ColoredFormatter, formatter_message
     from starkit.base.assemble_model import assemble_model
     from starkit.fix_spectrum1d import SKSpectrum1D
     import starkit.base.operations as operations
 
     import logging
 
+    FORMAT = "[$BOLD%(name)-20s$RESET][%(levelname)-18s]  %(message)s ($BOLD%(filename)s$RESET:%(lineno)d)"
+    COLOR_FORMAT = formatter_message(FORMAT, True)
+
+    logging.captureWarnings(True)
     logger = logging.getLogger('starkit')
     logger.setLevel(logging.INFO)
-    console_handler = logging.StreamHandler()
-    console_formatter = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_formatter = ColoredFormatter(COLOR_FORMAT)
     console_handler.setFormatter(console_formatter)
+
     logger.addHandler(console_handler)
+    logging.getLogger('py.warnings').addHandler(console_handler)
