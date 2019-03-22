3e1333b14c6e532a2a3befd90cc5b7672c8f901f
Soham Gaikwad <sohamvg11@gmail.com>
2019-03-22 18:13:26 +0530

    minor changes

--- ebv_from_coords.py
+++ ebv_from_coords.py
@@ -1,5 +1,5 @@
 '''
-Function to generate the value of extinction, E(B-V) from sky coordinates in Right Ascension (RA) and Declination (Dec) from SFD Dustmap 
+Function to generate the value of extinction E(B-V) from sky coordinates in Right Ascension (RA) and Declination (Dec) from SFD Dustmap. 
 Must have downloaded SFD Dustmap
 '''
 
@@ -8,16 +8,16 @@ from astropy.coordinates import SkyCoord
 from dustmaps.sfd import SFDQuery 
 import astropy.units as u
 
+'''
+PARAMETERS:
+rightAcsension: (int/float) angular distance of a particular point measured eastward along equator
+declination: (int/float) angular distance of a point north or south of the celestial equator
+-----------
+RETURNS:
+ebv: (magnitudes) color excess - degree of interstellar extinction/reddening
+'''
 def EBV_from_Coord(rightAscension,declination):
     coords = SkyCoord(ra=rightAscension*u.degree, dec=declination*u.degree, frame='icrs')
     sfd = SFDQuery()
     ebv = sfd(coords)
     return (ebv)
-
-
-
-'''
-Testing
-x = EBV_from_Coord(2,5)
-print("E(B-V) = %.3f" % x)
-'''
